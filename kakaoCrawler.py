71ded16a90d93cb32889f46bda8dc1a2dbd07fc7
snrntpa2z <snrntpa2z@naver.com>
2020-07-06 17:49:10 +0900

    issue: page 3이상의 경우 같은 데이터 반복 출력..

--- kakaoCrawler.py
+++ kakaoCrawler.py
@@ -3,13 +3,15 @@ import sys
 import json
 url = "https://dapi.kakao.com/v2/local/search/category.json?"
 apikey = "6663b13431da132c67c66562c3465c30"
-query = "CE7"
+query = "카페"
 page = 1
-r = requests.get( url, params = {'category_group_code':query}, headers={'Authorization' : 'KakaoAK ' + apikey } )
-js = simplejson.JSONEncoder().encode(r.json())
-r.json()
-jstring = json.dumps(r.json(), indent=4, ensure_ascii=False)
-f = open('kakaoCE7_'+str(page)+'.json', 'w', encoding="utf-8")
-f.write(jstring)
-f.close()
-print(r.json())
+while(page < 46):
+    r = requests.get( url, params = {'category_group_code': 'CE7' ,'category_group_name':query, 'page': page}, headers={'Authorization' : 'KakaoAK ' + apikey } )
+    js = simplejson.JSONEncoder().encode(r.json())
+    r.json()
+    jstring = json.dumps(r.json(), indent=4, ensure_ascii=False)
+    f = open('./kakao/data/kakaoCE7_'+str(page)+'.json', 'w', encoding="utf-8")
+    f.write(jstring)
+    f.close()
+    print(r.json())
+    page += 1

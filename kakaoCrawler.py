fe37678edcbd4c937f1a344a9545b329d4acca69
snrntpa2z <snrntpa2z@naver.com>
2020-07-06 18:03:58 +0900

    fix: 아무리 생각해도 전국 데이터 가져오려면 경도 위도로 데이터 값 가져와서 중복 되는 값 제거 해야 될 듯

--- kakaoCrawler.py
+++ kakaoCrawler.py
@@ -4,14 +4,17 @@ import json
 url = "https://dapi.kakao.com/v2/local/search/category.json?"
 apikey = "6663b13431da132c67c66562c3465c30"
 query = "카페"
-page = 1
-while(page < 46):
-    r = requests.get( url, params = {'category_group_code': 'CE7' ,'category_group_name':query, 'page': page}, headers={'Authorization' : 'KakaoAK ' + apikey } )
+number = 1
+x = 124.1
+y = 33.1
+while(x < 131.7):
+    r = requests.get( url, params = {'category_group_code': 'CE7' ,'category_group_name':query, 'x': x, 'y': y}, headers={'Authorization' : 'KakaoAK ' + apikey } )
     js = simplejson.JSONEncoder().encode(r.json())
     r.json()
     jstring = json.dumps(r.json(), indent=4, ensure_ascii=False)
-    f = open('./kakao/data/kakaoCE7_'+str(page)+'.json', 'w', encoding="utf-8")
+    f = open('./kakao/data/kakaoCE7_'+str(number)+'.json', 'w', encoding="utf-8")
     f.write(jstring)
     f.close()
     print(r.json())
-    page += 1
+    x += 0.6
+    number += 1

2ed22317bd5f8140dcf58011bbd0934960a68479
snrntpa2z@naver.com <snrntpa2z@naver.com>
2020-07-07 00:35:55 +0900

    add: Crawler ++

--- jsonParser.py
+++ jsonParser.py
@@ -1,9 +1,16 @@
 import json
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
 number = 1
 while(number < 3):
     with open('./data/kakaoCE7_'+str(number)+'.json') as json_file:
         json_data = json.load(json_file)
 
         for documents in json_data['documents']:
-            print(documents['place_url'])
+            context = ssl._create_unverified_context()
+            html = urlopen(documents['place_url'], context=context)
+            bsObject = BeautifulSoup(html, 'html.parser')
+            print(bsObject)
     number += 1
